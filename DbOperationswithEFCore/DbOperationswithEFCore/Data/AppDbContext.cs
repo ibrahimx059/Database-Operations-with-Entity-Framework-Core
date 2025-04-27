f4bc0cd665f2f7419a68b3c3a262d35041e242f0
ibrahim <ibraheemmughal789@gmail.com>
2025-04-27 15:22:48 +0500

    CRUD Operation Using Efcore

--- DbOperationswithEFCore/DbOperationswithEFCore/Data/AppDbContext.cs
+++ DbOperationswithEFCore/DbOperationswithEFCore/Data/AppDbContext.cs
@@ -5,8 +5,6 @@ namespace DbOperationswithEFCore.Data
 	public class AppDbContext : DbContext //Dbcontext is just a bridge b/w yr Application and Database and options areto configure the connection string, logging, lazy loading, etc.
 	{
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // This is a constructor that takes in DbContextOptions<AppDbContext> as a parameter and base is to send to Parent dbcontext class.
-
-
 		{
 		}
 
